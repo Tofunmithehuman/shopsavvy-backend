0f25e90ea23b2d57ebc78c7a052b8f1401ff28b2
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2023-11-23 19:17:38 +0100

    product routes

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ const io = new Server(server, {
 
 const User = require('./models/User')
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/users', userRoutes)
+app.use('/products', productRoutes);
 
 
 
