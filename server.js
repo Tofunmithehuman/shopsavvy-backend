93f3eb020541f7d45fa64e943b65ab33e27a8f79
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2023-12-20 21:28:57 +0100

    api

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const http = require('http');
-require('./connection');
-const server = http.createServer(app);
-const {Server} = require('socket.io');
-const io = new Server(server, {
-    cors: '*',
-    methods: '*'
-})
-
-
-const User = require('./models/User')
+const { Server } = require('socket.io');
+const mongoose = require('mongoose'); // Assuming you are using mongoose for connection
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 
+const app = express();
+const port = process.env.PORT || 3500;
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+// Assuming you have a separate connection.js file for MongoDB connection
+require('./connection');
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 
+// Define the User model here (assuming it's defined in models/User.js)
+// const User = require('./models/User');
 
-
-server.listen(8000, ()=> {
-    console.log('server running at port', 8000)
-})
+server.listen(port, "0.0.0.0", () => {
+    console.log('Server running at port', port);
+});
