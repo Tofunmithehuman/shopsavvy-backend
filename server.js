4333f9f36261817593a409d9e2fb63a354836bd2
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-01-07 19:04:54 +0100

    create and remove product route completed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { Server } = require('socket.io');
 const mongoose = require('mongoose'); // Assuming you are using mongoose for connection
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
+const imageRoutes = require("./routes/imageRoutes")
 
 const app = express();
 const port = process.env.PORT || 3500;
@@ -12,7 +13,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: '*',
-        methods: ['GET', 'POST'],
+        methods: "*",
     },
 });
 
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
+app.use('/images', imageRoutes);
 
 // Define the User model here (assuming it's defined in models/User.js)
 // const User = require('./models/User');
